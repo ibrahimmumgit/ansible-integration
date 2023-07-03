c0e4e52fa941b8358710c77e9014daa800745b99
Ibrahim K <68802843+ibrahimmumgit@users.noreply.github.com>
2023-07-03 16:21:10 +0530

    Update variables.tf

--- azure-3-tier-architecture/variables.tf
+++ azure-3-tier-architecture/variables.tf
@@ -1,96 +1,106 @@
-# variable "resource_group" {
-#     description = "This specifies the resource group of the virtual machine"
-#     type = string
-# }   
+// variable "resource_group" {
+//     description = "This specifies the resource group of the virtual machine"
+//     type = string
+// }   
 
-# variable "location" {
-#     description = "This specifies the location of the virtual machine" 
-#     default = "eastus"
-#     type = string
-# }
+variable "location" {
+     description = "This specifies the location of the virtual machine" 
+     default = "eastus"
+     type = string
+}
 
-# variable "vnet_cidr" {
-#     description = "This specifies the address apace of the web subnet"
-#     type = string
-# }
+// variable "vnet_cidr" {
+//     description = "This specifies the address apace of the web subnet"
+//     type = string
+// }
 
-# variable "web_subnet_cidr" {
-#     description = "This specifies the host name of the web-vm"
-#     type = string
-# }
+// variable "web_subnet_cidr" {
+//     description = "This specifies the host name of the web-vm"
+//     type = string
+// }
 
-# variable "app_subnet_cidr" {
-#     description = "This specifies the address apace of the app subnet"
-#     type = string
-# }
+// variable "app_subnet_cidr" {
+//     description = "This specifies the address apace of the app subnet"
+//     type = string
+// }
 
-# variable "db_subnet_cidr" {
-#     description = "This specifies the address apace of the app subnet"
-#     type = string
-# }
+// variable "db_subnet_cidr" {
+//     description = "This specifies the address apace of the app subnet"
+//     type = string
+// }
 
-# variable "web_subnet_id" {
-#     description = "This specifies the address space of the web subnet"
-#     type = string
-# }
+// variable "web_subnet_id" {
+//     description = "This specifies the address space of the web subnet"
+//     type = string
+// }
 
-# variable "app_subnet_id" {
-#     description = "This specifies the address space of the app subnet"
-#     type = string
-# }
+// variable "app_subnet_id" {
+//     description = "This specifies the address space of the app subnet"
+//     type = string
+// }
 
-# variable "db_subnet_id" {
-#     description = "This specifies the address space of the db subnet"
-#     type = string
-# }
+// variable "db_subnet_id" {
+//     description = "This specifies the address space of the db subnet"
+//     type = string
+// }
 
-# variable "web_host_name" {
-#     description = "This specifies the host name of the web-vm"
-#     type = string
-# }
+variable "web_host_name" {
+    description = "This specifies the host name of the web-vm"
+    type = string
+    default = "web_3tier"
+}
 
-# variable "web_username" {
-#     description = "This specifies the username of the web-vm"
-#     type = string
-# }
+variable "web_username" {
+    description = "This specifies the username of the web-vm"
+    type = string
+    default = "web_user"
 
-# variable "web_os_password" {
-#     description = "This specifies the passoword of the web-vm"
-#     type = string
-# }
+}
 
-# variable "app_host_name" {
-#     description = "This specifies the host name of the app-vm"
-#     type = string
-# }
+variable "web_os_password" {
+    description = "This specifies the passoword of the web-vm"
+    type = string
+    default = "Password!234"
+}
 
-# variable "app_username" {
-#     description = "This specifies the username of the app-vm"
-#     type = string
-# }
+variable "app_host_name" {
+    description = "This specifies the host name of the app-vm"
+    type = string
+    default = "app_3tier"
+}
 
-# variable "app_os_password" {
-#     description = "This specifies the password of the app-vm"
-#     type = string
-# }
+variable "app_username" {
+    description = "This specifies the username of the app-vm"
+    type = string
+    default = "app_user"
+}
 
-# variable "dev_database" {
-#     description = "This specifies the database name"
-#     type = string
-# }
+variable "app_os_password" {
+    description = "This specifies the password of the app-vm"
+    default = "Password!234"
+    type = string
+}
 
-# variable "dev_database_version" {
-#     description = "This specifies the version of the database"
-#     type = string
-# }
+variable "dev_database" {
+    description = "This specifies the database name"
+    type = string
+    dev="db_3tier"
+}
 
-# variable "dev_database_admin" {
-#     description = "This specifies the database administrator login"
-#     type = string
-# }
+// variable "dev_database_version" {
+//     description = "This specifies the version of the database"
+//     type = string
+// }
 
-# variable "dev_database_password" {
-#     description = "This specifies the administrator login password"
-#     type = string
-# }
+variable "dev_database_admin" {
+    description = "This specifies the database administrator login"
+    type = string
+    default = "dbtest"
+}
+
+variable "dev_database_password" {
+    description = "This specifies the administrator login password"
+    type = string
+    default = "Password!234"
+}
 
